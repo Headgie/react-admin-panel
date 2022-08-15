e945b7b64e8738416a6b820f839a31feb0ab5535
Headgie <headgy@yandex.ru>
2022-08-16 01:27:29 +0300

    react create page

---
+++ app/src/components/editor/index.js
@@ -0,0 +1,2 @@
+import Editor from "./editor.js";
+export default Editor;
--- app/src/main.js
+++ app/src/main.js
@@ -1,23 +1,11 @@
-import $ from "jquery";
+// import $ from "jquery";
 
-function getPageList() {
-  $("h1").remove();
-  $.get("./api", data => {
-  data.forEach(file => {
-    $("body").append(`<h1>${file}</h1>`)
-  });
-}, "JSON")
-}
+import React from "react";
+import  ReactDOM  from "react-dom";
+import Editor from "./components/editor/editor.js";
 
-getPageList();
 
-$("button").click(() => {
-  $.post("./api/createNewPage.php", {
-    "name": $("input").val()
-  }, () => {
-    getPageList();
-  })
-  .fail(() => {
-    alert("Страница уже существует");
-  })
-})
+
+
+ReactDOM.render(<Editor/>, document.getElementById("root"));
+
